78eeb60431401e8481fd966d4c15732ffd9ad08a
mohammadfaidi <31220054+mohammadfaidi@users.noreply.github.com>
2021-08-07 04:51:36 -0700

    Update notcomplate.dart

--- lib/Screens/notcomplate.dart
+++ lib/Screens/notcomplate.dart
@@ -18,7 +18,7 @@ class ScreenB extends StatelessWidget {
                     itemCount: TP.basketItems.length,
                     itemBuilder: (context, index) {
                       return  Card(
-                        child: ListTile(
+                        child:TP.basketItems[index].chekvalueText?null: ListTile(
                           title:TP.basketItems[index].chekvalueText?null:Text(TP.basketItems[index].valueText,style: TextStyle(fontSize: 17,color: Colors.red,fontWeight: FontWeight.bold)
                           ),
                           trailing: TP.basketItems[index].chekvalueText?null:Icon(Icons.favorite_border,color: Colors.red,),
